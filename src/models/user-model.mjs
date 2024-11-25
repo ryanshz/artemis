f55f3b3d00078b0f831e5bf30a93847420860275
ryanshz <92750669+ryanshz@users.noreply.github.com>
2024-11-25 18:37:50 -0500

    messed with user-model for a bit, swapped to modular version from firebse docs

--- src/models/user-model.mjs
+++ src/models/user-model.mjs
@@ -1,7 +1,10 @@
+import { getDatabase, ref, set } from "firebase/database";
+
 function writeUserData(userId, name, email, imageUrl) {
-    firebase.database().ref('users/' + userId).set({
-        username: name,
-        email: email,
-        profile_picture : imageUrl
-    });
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
 }
